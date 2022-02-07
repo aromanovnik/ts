ac3173639e66a4334b1d314645b642349930e668
Aleksey Romanov <badkidddd@gmail.com>
2022-02-07 17:40:22 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchStubBlock } from './search-results.js';
+import { renderUserBlock } from './user.js';
+import { renderToast } from './lib.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('', './img/avatar.png', 0);
+  renderSearchFormBlock(new Date(), new Date());
+  renderSearchStubBlock();
+  renderToast(
+    { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
+    {
+      name: 'Понял',
+      handler: () => {
+        console.log('Уведомление закрыто');
+      },
+    },
+  );
+});
